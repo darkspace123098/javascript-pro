e02687705ef58f25dad350efbe02274f8b3effdd
Mohammed Ali <mohammedali777444@gmail.com>
2024-10-18 20:06:41 +0530

    array

--- second.js
+++ second.js
@@ -1,6 +1,17 @@
 // Array 
+const arr = ["java",74,true]
+arr.push("why push")
+arr.pop()
+console.log(arr);
 
-const myArr = [];
+arr.unshift("why shifted")
+arr.shift()
+console.log(arr);
 
 
-console.log(myArr);
+// const myArr = [];
+// myArr[1] = 5
+// const set = new Date("10-2-2024")
+// const get = set.toDateString()
+// myArr[2] = get
+// console.log(myArr);
