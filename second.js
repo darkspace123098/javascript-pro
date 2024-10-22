76e2fbb01023ae94d84fe362273e616f4f0937e3
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-10-22 15:27:07 +0000

    update second

--- second.js
+++ second.js
@@ -1,12 +1,16 @@
 // Array 
-const arr = ["java",74,true]
-arr.push("why push")
-arr.pop()
-console.log(arr);
 
-arr.unshift("why shifted")
-arr.shift()
-console.log(arr);
+// const arr = ["java",74,true,98,"go"]
+// arr.push("why push")
+// arr.pop()
+// console.log(arr);
+// console.log(arr.includes("go"));
+
+
+
+// arr.unshift("why shifted")
+// arr.shift()
+// console.log(arr);
 
 
 // const myArr = [];
@@ -15,6 +19,15 @@ console.log(arr);
 // const get = set.toDateString()
 // myArr[2] = get
 // console.log(myArr);
+
+//Join
+// const arr1 = [58,25,61]
+// const arr2 = arr1.join();
+// console.log(arr2);
+// console.log(typeof arr2);
+
+
+
 
 
 
