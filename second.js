00ad71a8cda9ef1b159492dc3717f6231f3360f0
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-10-22 16:30:32 +0000

    object update

--- second.js
+++ second.js
@@ -37,8 +37,8 @@
 // Array Part2 
 
 
-const marvel_heros = ["thor","ironman","spiderman"]
-const dc_heros = ["superman","flash","batman"]
+// const marvel_heros = ["thor","ironman","spiderman"]
+// const dc_heros = ["superman","flash","batman"]
 // marvel_heros.push(dc_heros)
 // marvel_heros.concat(dc_heros)
 // const all = marvel_heros.concat(dc_heros)
@@ -46,11 +46,37 @@ const dc_heros = ["superman","flash","batman"]
 // console.log(all);
 
 /* Another Method to concat - Spread Operator*/
-const all_heros = [...marvel_heros,...dc_heros]
-console.log(all_heros);
+// const all_heros = [...marvel_heros,...dc_heros]
+// console.log(all_heros);
 
+// flat
+// const unknown_arr = [1,2,3,[4,5,6],7,[8,9,0,[5,6,[8,9]]]]
+// const flater = unknown_arr.flat()
+// const multi_flater = unknown_arr.flat(Infinity)
+// console.log(unknown_arr);
+// console.log(flater);
+// console.log(multi_flater);
 
+// Checkerr
+// console.log(Array.isArray("wordpress"));
 
+// console.log(Array.from("jacob"));
+// const obj = {
+//     name : "joseph"
+// }
+// console.log(Array.from(obj.name));
+
+// Object - The Nitro
+const jsuser = {
+    name: "joseph",
+    age: 18,
+    married: false
+}
+console.log(jsuser.name);
+jsuser.add = "karnataka"
+Object.freeze(jsuser)
+jsuser.email = "userjos@gmail.com"
+console.log(jsuser);
 
 
 
