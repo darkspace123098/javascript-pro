e5fe19b673cdd6b39acfd4d649cc36cf95890ff8
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-10-22 16:08:12 +0000

    update array2

--- second.js
+++ second.js
@@ -24,7 +24,30 @@
 // const arr1 = [58,25,61]
 // const arr2 = arr1.join();
 // console.log(arr2);
+// console.log("a",arr1);
 // console.log(typeof arr2);
+
+// Slice and splice
+// const myarr = [1,2,3,4,5]
+// const spli = myarr.splice(0,5)      //  it will effect the original array
+// const sli = myarr.slice(0,4)
+// console.log(spli);
+// console.log(myarr);
+
+// Array Part2 
+
+
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+// marvel_heros.push(dc_heros)
+// marvel_heros.concat(dc_heros)
+// const all = marvel_heros.concat(dc_heros)
+// console.log(marvel_heros);
+// console.log(all);
+
+/* Another Method to concat - Spread Operator*/
+const all_heros = [...marvel_heros,...dc_heros]
+console.log(all_heros);
 
 
 
