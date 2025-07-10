5750f9555afa6c94d32ac6148fd17daade72e426
Dark Space <darkspace123098@gmail.com>
2025-07-10 18:09:31 +0530

    intermidiate javascript done

---
+++ ninth.js
@@ -0,0 +1,36 @@
+// Lexical scoping and closure
+
+
+// function one()
+// {
+//     let x = 70
+    
+//     function two()
+//     {
+//         console.log("two",x);
+//         function three()
+//         {
+//             console.log("three",x);
+//         }
+//         three()
+        
+//     }
+//     two()
+// }
+
+// one()
+
+
+
+// function outer()
+// {
+//     console.log("outer lexical");
+//     let y = 90
+//     function inner()
+//     {
+
+//     }
+//     return inner
+// }
+
+// outer()
